a93066edc36797a800f8a12b03ab0ab61d94151b
Kingjha13 <avnishjha277@gmail.com>
2026-09-06 02:58:52 +0530

    Android and Backend Created

---
+++ Mernchant_Agent_Backend/src/main/java/com/mernchant_agent_backend/MernchantAgentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mernchant_agent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MernchantAgentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MernchantAgentBackendApplication.class, args);
+    }
+
+}
